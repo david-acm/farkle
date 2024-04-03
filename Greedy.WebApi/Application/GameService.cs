ad092b3bb728dc62b59c0e00b981a6a351e13d7e
davidchaparro <davidacm7@outlook.com>
2024-04-03 19:21:52 -0400

    Framework version update

--- Greedy.WebApi/Application/GameService.cs
+++ Greedy.WebApi/Application/GameService.cs
@@ -5,23 +5,17 @@ using static Eventuous.ExpectedState;
 namespace Greedy.WebApi.Application;
 
 public class GameService : CommandService<Game, GameState, GameId> {
-  public GameService(IAggregateStore store, IConfiguration config) : base(store)
+  public GameService(IAggregateStore store) : base(store)
   {
     On<Command.StartGame>()
       .InState(New)
       .GetId(cmd => new GameId(cmd.GameId))
       .Execute((game, cmd) => game.Start(cmd));
     
-    // Controlador
-    // [Authorize(Player)]
-    // public async Task<IHttpResponse> Game(StartGame dto) { // On<Command.StartGame>()
-    //   var id = new GameId(dto.GameId) // GetId(cmd => new GameId(cmd.GameId))
-    //   var game = new Game(id); // CommandService<Game
-    //   game.Start(dto) // .Execute((game, cmd) => game.Start(cmd))
-    // }
-
-    On<Command.JoinPlayer>().InState(Existing).GetId(cmd => new GameId(cmd.GameId)).
-      Execute((game, cmd) => game.JoinPlayer(cmd));
+    On<Command.JoinPlayer>()
+      .GetId(cmd => new GameId(cmd.GameId))
+      .InState(Existing)
+      .Execute((game, cmd) => game.JoinPlayer(cmd));
 
     On<Command.RollDice>().InState(Existing).GetId(cmd => new GameId(cmd.GameId)).
       Execute((game, cmd) => game.RollDiceV2(cmd));
